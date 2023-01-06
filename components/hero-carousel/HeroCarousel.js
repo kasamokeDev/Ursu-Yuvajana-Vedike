567a2200f8d94401911a977268a58beb10183868
Niraj Kumar Pandey <nirajpandey034@gmail.com>
2023-01-06 23:46:57 +0530

    added carousel

--- components/hero-carousel/HeroCarousel.js
+++ components/hero-carousel/HeroCarousel.js
@@ -1,9 +1,37 @@
-import React from 'react'
+/* eslint-disable @next/next/no-img-element */
+import React from 'react';
+import { Container } from '@mui/material';
+import Carousel from 'react-bootstrap/Carousel';
 
 function HeroCarousel() {
   return (
-    <div>HeroCarousel</div>
-  )
+    <Carousel fade slide style={{ width: '100vw' }}>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="/assets/images/carouselImage1.jpg"
+          alt="First slide"
+          style={{ maxWidth: '100%', height: '15rem', objectFit: 'cover' }}
+        />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="/assets/images/carouselImage2.jpg"
+          alt="Second slide"
+          style={{ maxWidth: '100%', height: '15rem', objectFit: 'cover' }}
+        />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="/assets/images/carouselImage3.jpg"
+          alt="Third slide"
+          style={{ maxWidth: '100%', height: '15rem', objectFit: 'cover' }}
+        />
+      </Carousel.Item>
+    </Carousel>
+  );
 }
 
-export default HeroCarousel
+export default HeroCarousel;
