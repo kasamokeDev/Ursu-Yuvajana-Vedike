544817f6e464e6693da9a86cdbecf5794f99a98d
Niraj Kumar Pandey <nirajpandey034@gmail.com>
2023-01-09 23:05:43 +0530

    header ui modification

--- components/header/Header.js
+++ components/header/Header.js
@@ -5,23 +5,39 @@ import style from './style.module.css';
 
 function Header() {
   return (
-    <Container maxWidth="sm">
-      <Grid container style={{alignItems: 'center'}}>
-        <Grid item xs={3} md={4} className={style.headerImageContainer}>
+    <Container maxWidth="xs">
+      <Grid
+        container
+        style={{ alignItems: 'center' }}
+        spacing={{ xs: 2, md: 1 }}
+      >
+        <Grid item xs={2} md={2} className={style.headerImageContainer}>
           <Box
             component="img"
             sx={{
-              height: 233,
-              width: 350,
-              maxHeight: { xs: 100, md: 100 },
-              maxWidth: { xs: 100, md: 100 },
+              height: 70,
+              width: 70,
+              maxHeight: { xs: 50, md: 50 },
+              maxWidth: { xs: 50, md: 50 },
+              borderRadius: '50%',
+              cursor: 'pointer',
             }}
             alt="Ursu Yuvajana Vedike"
             src="/assets/logo/OfficialLogo.png"
+            onClick={() => {
+              window.open('/', '_self');
+            }}
           ></Box>
         </Grid>
-        <Grid item xs={8} md={8} className={style.headerTextContainer}>
-          <Typography className={style.headerText}>Ursu Yuvajana Vedike</Typography>
+        <Grid item xs={10} md={10} className={style.headerTextContainer}>
+          <Typography
+            className={style.headerText}
+            onClick={() => {
+              window.open('/', '_self');
+            }}
+          >
+            Ursu Yuvajana Vedike
+          </Typography>
         </Grid>
       </Grid>
     </Container>
